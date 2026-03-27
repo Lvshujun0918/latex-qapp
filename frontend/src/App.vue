4bfc8ca13eca531d5d445db90973a005fa964cbd
lvshujun <Lvshujun0918@163.com>
2026-03-27 21:01:49 +0800

    feat(auth): 增加阻拦路由

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,5 +5,13 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.fetchMe();
+});
 </script>
